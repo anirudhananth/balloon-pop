208d3ddfd97c0ec6146881e54b2a513793bf522c
Anirudh Ananthasubramaniam <anirudhananth99@gmail.com>
2024-01-29 00:45:38 -0500

    Timing changes

--- Assets/Scripts/Inflate.cs
+++ Assets/Scripts/Inflate.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Inflate : MonoBehaviour
 {
     public GameOverScreen gameOverScreen;
-    private float maxTime = 3.25f;
+    private float maxTime = 3.75f;
     private float timer = 0.0f;
     private bool started = false;
     private bool gameOver = false;
@@ -16,21 +17,24 @@ public class Inflate : MonoBehaviour
     public AudioClip inflateSound;
     public AudioClip popSound;
     private int score = 0;
+    private DateTime _start;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _start = DateTime.Now;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(balloon && Input.GetKeyDown(KeyCode.Mouse0) && !audioSource.isPlaying && !gameOver) {
+            _start = DateTime.Now;
             audioSource.PlayOneShot(inflateSound);
             started = true;
         }
         if(balloon && Input.GetKey(KeyCode.Mouse0) && !gameOver) {
-            timer += Time.deltaTime;
+            TimeSpan _time = DateTime.Now - _start;
+            timer = _time.Seconds + _time.Milliseconds / 1000f;
             if(timer < maxTime) {
                 balloon.transform.localScale += new Vector3(0.0004f, 0.0006f, 0f);
                 balloon.transform.position += new Vector3(0f, 0.0003f, 0f);
@@ -67,8 +71,8 @@ public class Inflate : MonoBehaviour
     void CreateAndThrow(GameObject go) {
         GameObject b = Instantiate(go, balloon.transform.position, balloon.transform.rotation);
         float randomX, randomY;
-        randomX = Random.Range(-5f, 5f);
-        randomY = Random.Range(4f, 6f);
+        randomX = UnityEngine.Random.Range(-5f, 5f);
+        randomY = UnityEngine.Random.Range(4f, 6f);
         b.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(randomX, randomY), ForceMode2D.Impulse);
     }
 
